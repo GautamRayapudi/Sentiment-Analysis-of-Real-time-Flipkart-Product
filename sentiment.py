99e24602c0bb21847df20df8059da0677cbcaadb
Gautam <113839091+GautamRayapudi@users.noreply.github.com>
2024-05-13 03:15:02 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,24 +1,3 @@
-import numpy as np
-import pandas as pd
-import re
-import emoji
-from textblob import TextBlob
-import nltk
-from nltk.tokenize import word_tokenize,sent_tokenize
-nltk.download('punkt')
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('vader_lexicon')
-from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
-from wordcloud import WordCloud
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from sklearn.metrics import accuracy_score,f1_score
 import streamlit as st
 import pickle
 result = None
@@ -29,7 +8,7 @@ import os
 
 current_dir = os.path.dirname(__file__)
 
-pickle_file_path = os.path.join(current_dir, "sentiment_yonex.pkl")
+pickle_file_path = os.path.join(current_dir, "sentiment_yonex2.pkl")
 
 with open(pickle_file_path, "rb") as f:
     model = pickle.load(f)
